7c4720ae3907201e99534ee2887eaaac7b068578
xqcqvc <136810553+xqcqvc@users.noreply.github.com>
2024-02-07 15:19:55 +0800

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -75,11 +75,10 @@ checkpoint = torch.load(model_path + '/' + 'checkpoint.pth.tar')
 model.load_state_dict(checkpoint['state_dict'])
 
 model.to(device)
-model.eval()  # 切换到评估模式
+model.eval()  
 
-# 收集特征向量的代码逻辑
 feature_vectors = []
-all_labels = []  # 改动之处：初始化用于收集所有标签的列表
+all_labels = []  
 
 with torch.no_grad():
     for data in train_loader:
@@ -87,23 +86,17 @@ with torch.no_grad():
         inputs = inputs.to(device)
         feature_vector = model(inputs, filter_size, filter_num, pool_size, if_bn, cnndrop_out, rnn_size, fc_size)
         
-        # 将特征向量从 GPU 转移到 CPU 并转换为 NumPy 数组
         feature_vector = feature_vector.cpu().numpy()
         feature_vectors.append(feature_vector)
-        all_labels.append(labels.cpu().numpy())  # 改动之处：收集当前批次的标签
+        all_labels.append(labels.cpu().numpy())  
 
 feature_vectors = np.concatenate(feature_vectors, axis=0)
-all_labels = np.concatenate(all_labels, axis=0)  # 改动之处：将所有批次的标签合并
+all_labels = np.concatenate(all_labels, axis=0)  
 
 def tSNE1(vectors,labels):
-    # t-SNE 需要花费一些时间来运行，尤其是对于较大的数据集
-    # 适当地设置 `n_iter` 和 `perplexity` 可以改善结果和运行时间
     tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
     tsne_results = tsne.fit_transform(feature_vectors)
-    # 创建一个绘图
     plt.figure(figsize=(10, 8))
-    # 使用 seaborn 的方便性创建散点图
-    # all_labels 是标签列表：0 代表负样本，1 代表正样本
     sns.scatterplot(
         x=tsne_results[:, 0], y=tsne_results[:, 1],
         hue=all_labels,
@@ -114,22 +107,17 @@ def tSNE1(vectors,labels):
     plt.savefig('Result/4mC_C.equisetifolia_tSNE.png')
 
 def UMAP1(vectors,labels):
-    # 使用UMAP进行降维
     umap_embeddings = umap.UMAP().fit_transform(feature_vectors)
 
-    # 绘制散点图
     plt.scatter(umap_embeddings[:, 0], umap_embeddings[:, 1], c=all_labels)
     plt.colorbar()
     plt.savefig('Result/4mC_C.equisetifolia_tSNE.png')
 
 def PCA1(vectors,labels):
-    # 假设 feature_vectors 的维度为 (n_samples, n_features, n_dimensions)
-    # 首先将其 reshape 成 (n_samples, n_features * n_dimensions)
     #reshaped_feature_vectors = feature_vectors.reshape(feature_vectors.shape[0], -1)
     pca = PCA(n_components=2)
     pca_representation = pca.fit_transform(feature_vectors)
 
-    # 改动之处：这里 all_labels 而不是 labels
     plt.scatter(pca_representation[all_labels == 1, 0], pca_representation[all_labels == 1, 1], label='Positive Samples',s=10)
     plt.scatter(pca_representation[all_labels == 0, 0], pca_representation[all_labels == 0, 1], label='Negative Samples',s=10)
 
